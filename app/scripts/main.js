491b6af210a870e8e93fda41bada01281020d0a5
agent1red <darklen14@gmail.com>
2017-01-07 21:18:18 -0800

    added an apple image and wrote a scroll fade scipt

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,4 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(window).scroll(function(){
+    $(".jumbotron > h1, .lead, .btn ").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
