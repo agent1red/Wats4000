fb3bc373810971434bde50c8233753c8954bf733
agent1red <darklen14@gmail.com>
2017-01-08 11:23:12 -0800

    Updated the scss to show h1 in green

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
 $(window).scroll(function(){
     $(".jumbotron > h1, .lead, .btn ").css("opacity", 1 - $(window).scrollTop() / 250);
   });
+  
